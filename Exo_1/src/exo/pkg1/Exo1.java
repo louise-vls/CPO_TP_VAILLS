6268171e8a656ecf21fccc697ae971c8af7030f2
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-09-18 15:27:21 +0200

    creation de l'exo 1

---
+++ Exo_1/src/exo/pkg1/Exo1.java
@@ -0,0 +1,23 @@
+/*
+*louise
+*TDA
+*TP0 Exercice 1
+*18 Septembre
+*/
+package exo.pkg1;
+
+/**
+ *
+ * @author louis
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        
+    }
+    
+}
