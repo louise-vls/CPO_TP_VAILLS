171e97c7c35fe8fa79dfda5eb4c3d75f0811925d
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-09-25 12:56:52 +0200

    Envoie de l'exo 1

--- Exo_1/src/exo/pkg1/Exo1.java
+++ Exo_1/src/exo/pkg1/Exo1.java
@@ -6,6 +6,8 @@
 */
 package exo.pkg1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author louis
@@ -17,7 +19,18 @@ public class Exo1 {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour");
+        System.out.println("Au revoir");
+        int i;
+        i=20;
+        i=i+1;
         
+        String prenom;
+Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, quel est votre prenom ?");
+prenom = sc.nextLine();
+   System.out.println("bonjour "+prenom);
+    
     }
     
 }
