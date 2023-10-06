02066d265ad0a7c5a39e8a382d2b79005b832df9
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-06 08:22:10 +0200

    Envoie de l'exo 1 TD2

---
+++ TP2/src/bière_vaills/BouteilleBière.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bière_vaills;
+
+/**
+ *
+ * @author louis
+ */
+public class BouteilleBière {
+    
+}
