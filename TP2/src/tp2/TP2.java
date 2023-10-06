8827b443cffed90c30ca59953917e0fda546cfc9
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-06 15:14:07 +0200

    Envoie de l'exo 1 TD2

--- TP2/src/tp2/TP2.java
+++ TP2/src/tp2/TP2.java
@@ -16,7 +16,41 @@ public class TP2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+    BouteilleBière uneBiere = new BouteilleBière("Cuvée des trolls", 
+    7.0 ,"Dubuisson") ;
+    uneBiere.lireEtiquette();
+    uneBiere.Décapsuler();
+    System.out.println(uneBiere) ;
+    
+    BouteilleBière deuxBiere = new BouteilleBière("Leffe", 
+    6.6 ,"Abbaye de Leffe") ;
+    deuxBiere.lireEtiquette();
+    deuxBiere.Décapsuler();
+    System.out.println(deuxBiere) ;
+
+    BouteilleBière troisBiere = new BouteilleBière("Corona", 
+    7.8 ,"Let's goooo!") ;
+    troisBiere.lireEtiquette();
+    troisBiere.Décapsuler();
+    System.out.println(troisBiere) ;
+    
+    BouteilleBière quatreBiere = new BouteilleBière("COCORICOOOOOO", 
+    7.2 ,"Pablo") ;
+    quatreBiere.lireEtiquette();
+    quatreBiere.Décapsuler();
+    System.out.println(quatreBiere) ;
+    
+    BouteilleBière cinqBiere = new BouteilleBière("No No Jose", 
+    7.1 ,"Josianne et Jaqueline") ;
+    cinqBiere.lireEtiquette();
+    cinqBiere.Décapsuler();
+    System.out.println(cinqBiere) ;
+    
+    
+    
+  
+
+
     }
     
 }
