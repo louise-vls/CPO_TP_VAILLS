0ed145476415a51ccba00ba2cff2a56b97eab97b
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-06 15:23:44 +0200

    Envoie de l'exo 1 TD2

--- TP2/src/tp2/TP2.java
+++ TP2/src/tp2/TP2.java
@@ -19,36 +19,37 @@ public class TP2 {
     BouteilleBière uneBiere = new BouteilleBière("Cuvée des trolls", 
     7.0 ,"Dubuisson") ;
     uneBiere.lireEtiquette();
+    System.out.println(uneBiere) ;
     uneBiere.Décapsuler();
     System.out.println(uneBiere) ;
     
     BouteilleBière deuxBiere = new BouteilleBière("Leffe", 
     6.6 ,"Abbaye de Leffe") ;
     deuxBiere.lireEtiquette();
+    System.out.println(deuxBiere) ;
     deuxBiere.Décapsuler();
     System.out.println(deuxBiere) ;
 
     BouteilleBière troisBiere = new BouteilleBière("Corona", 
     7.8 ,"Let's goooo!") ;
     troisBiere.lireEtiquette();
+    System.out.println(troisBiere) ;
     troisBiere.Décapsuler();
     System.out.println(troisBiere) ;
     
     BouteilleBière quatreBiere = new BouteilleBière("COCORICOOOOOO", 
     7.2 ,"Pablo") ;
     quatreBiere.lireEtiquette();
+    System.out.println(quatreBiere) ;
     quatreBiere.Décapsuler();
     System.out.println(quatreBiere) ;
     
     BouteilleBière cinqBiere = new BouteilleBière("No No Jose", 
     7.1 ,"Josianne et Jaqueline") ;
     cinqBiere.lireEtiquette();
+    System.out.println(cinqBiere) ;
     cinqBiere.Décapsuler();
     System.out.println(cinqBiere) ;
-    
-    
-    
-  
 
 
     }
