36fa4636527e8776081dfb72469026c160072925
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-06 08:21:44 +0200

    Envoie de l'exo 1 TD2

---
+++ TP2/src/tp2/TP2.java
@@ -0,0 +1,22 @@
+/*
+*louise
+*TDA
+*TP2 EXO 1
+*04 Octobre
+ */
+package tp2;
+
+/**
+ *
+ * @author louis
+ */
+public class TP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
