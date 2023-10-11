441a7173207062b63802453422510b718b77195b
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-11 11:14:23 +0200

    Envoie de l'exo 3 TD2

---
+++  TP2_manip_NOM1/src/tp2_manip_nom1/Tartiflette.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_nom1;
+
+/**
+ *
+ * @author louis
+ */
+public class Tartiflette {
+    int nbCalories;
+    public Tartiflette (int nb){
+       nbCalories=nb;
+   }
+}
