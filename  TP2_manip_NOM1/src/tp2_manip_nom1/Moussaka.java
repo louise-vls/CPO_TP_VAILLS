818eb2715e5e4557cb040904fb181c1f9687d9dc
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-11 11:14:16 +0200

    Envoie de l'exo 3 TD2

---
+++  TP2_manip_NOM1/src/tp2_manip_nom1/Moussaka.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_nom1;
+
+/**
+ *
+ * @author louis
+ */
+public class Moussaka {
+   int nbCalories;
+   public Moussaka (int nb){
+       nbCalories=nb;
+   }
+}
