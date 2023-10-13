e5d9f6f7fd61656da954b7ed7faedcdeb1a317b5
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-13 17:45:47 +0200

    Envoie de l'exo 4 TD2

---
+++ TP2_relation_1_VAILLS/src/tp2_relation_1_vaills/Voiture.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_vaills;
+
+/**
+ *
+ * @author louis
+ */
+public class Voiture {
+    String Modele;
+    String Marque;
+    int PuissanceCV;
+    Personne Proprietaire;
+    
+    public Voiture(String unModele, String uneMarque,int unePuissanceCV){
+        Modele=unModele;
+        Marque=uneMarque;
+        PuissanceCV=unePuissanceCV;
+        Proprietaire=null;    
+    }
+    
+    @Override
+public String toString () {
+    return Modele+" "+Marque+" "+PuissanceCV;
+
+       
+    }
+}
