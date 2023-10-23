630be5bf77c792e1677a7658f1f2e9b67e9f5f19
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-23 17:06:36 +0200

    Envoie de l'exo 2 TD3

--- tp3/src/tp3/Tp3.java
+++ tp3/src/tp3/Tp3.java
@@ -4,6 +4,7 @@
  */
 package tp3;
 import armes.*;
+import Personnages.*;
 import java.util.ArrayList;
 /**
  *
@@ -20,11 +21,34 @@ public class Tp3 {
         Baton Chene = new Baton("Chêne", 4, 5);
         Baton Charme = new Baton("Charme", 5, 6);
         
-        ArrayList<Arme> arme= new ArrayList<>();
-        arme.add(Excalibur);
-        arme.add(Durandal);
-        arme.add(Chene);
-        arme.add(Charme);
+        System.out.print(Excalibur);
+        System.out.print(Durandal);
+        System.out.print(Chene);
+        System.out.print(Charme);
+        
+        ArrayList<Arme> TabAarme= new ArrayList<>();
+        TabAarme.add(Excalibur);
+        TabAarme.add(Durandal);
+        TabAarme.add(Chene);
+        TabAarme.add(Charme);
+        
+        Magicien mag1= new Magicien("Gandalf",65,true);
+        Magicien mag2= new Magicien("Garcimore",44,false);
+        Guerrier g1= new Guerrier("Conan",78, false);
+        Guerrier g2= new Guerrier("Lannister",45, true);
+        
+        ArrayList<Personnage> personnage=new ArrayList<>();
+        personnage.add(mag1);
+        personnage.add(mag2);
+        personnage.add(g1);
+        personnage.add(g2);
+        
+        for (int i=0;i<personnage.size();i++){
+          System.out.println(Personnage.get(i));
+        }
+        
+        
+        
         
     }
     
