1a061e4c837b69a686bbbeb71bc793e5c3459100
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-23 11:13:59 +0200

    Envoie de l'exo 1 TD3

---
+++ tp3/src/armes/Arme.java
@@ -0,0 +1,31 @@
+package armes;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author louis
+ */
+public class Arme {
+    String Nom;
+    int NiveauAttaque=100;
+    
+    public Arme(String nom, int niveauAttaque){
+        Nom=nom;
+        NiveauAttaque=niveauAttaque;
+       
+        
+    }
+    
+        @Override
+public String toString () {
+    String chaine;
+    chaine= Nom+""+NiveauAttaque;
+    return chaine;
+}
+
+    
+}
