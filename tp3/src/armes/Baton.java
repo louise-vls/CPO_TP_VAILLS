8f76ef96522d6feac3557a4f97eada20e322f4a1
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-23 11:14:07 +0200

    Envoie de l'exo 1 TD3

---
+++ tp3/src/armes/Baton.java
@@ -0,0 +1,25 @@
+package armes;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author louis
+ */
+public class Baton extends Arme {
+    int Age;
+
+    public Baton(String nom, int niveauAttaque, int age) {
+        super(nom, niveauAttaque);
+        Age=age;
+    }
+           @Override
+public String toString () {
+    String chaine;
+    chaine= Nom+""+NiveauAttaque;
+    return chaine;
+}
+}
