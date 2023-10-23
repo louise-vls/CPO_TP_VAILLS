3fc3411e4821ee51ba3fb38b2587803aa2c150cd
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-23 11:13:30 +0200

    Envoie de l'exo 1 TD3

---
+++ tp3/src/tp3/Tp3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3;
+import armes.*;
+/**
+ *
+ * @author louis
+ */
+public class Tp3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Epee Excalibur = new Epee("Excalibur",7,5);
+        Epee Durandal = new Epee("Durandal",4,7);
+        Baton Chene = new Baton("Chêne", 4, 5);
+        Baton Charme = new Baton("Charme", 5, 6);
+        
+    }
+    
+    
+}
