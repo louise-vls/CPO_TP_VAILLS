328cdc45742a7d9e7db127cbf1e83e81b1f26c67
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-24 22:48:49 +0200

    Envoie de l'exo 2 TD3

---
+++ tp3/src/EtreVivant/etreVivant.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package EtreVivant;
+
+/**
+ *
+ * @author louis
+ */
+public interface etreVivant {
+    void seFatiguer();
+    boolean estVivant();
+    void estAttaque(int point);
+}
