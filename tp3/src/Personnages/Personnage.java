bb914b0e55910db458d333e30072c4a0a0ca1f99
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-24 15:20:30 +0200

    Envoie de l'exo 3 TD3

--- tp3/src/Personnages/Personnage.java
+++ tp3/src/Personnages/Personnage.java
@@ -3,36 +3,74 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Personnages;
-
+import armes.*;
+import java.util.ArrayList;
 /**
  *
  * @author louis
  */
 public abstract class Personnage {
+    static int nombrePersonnages=0;
+    int nombreGuerriers=0;
+    int nombreMagiciens=0;
+            
     String nom;
     int niveauVie=100;
-  
+    ArrayList<Arme> inventaire;
+    Arme armeEnMain;
+    
        
    
     
     public Personnage( String Nom, int NiveauVie){
         this.nom=Nom;
         this.niveauVie=NiveauVie;
+        this.inventaire= new ArrayList<>();
+        this.armeEnMain=null;
+        nombrePersonnages ++;
+        
         
                 }
-    public String getNom(){
-        return nom;
+    
+    public void ajouterArme(Arme arme){
+        if (inventaire.size()<5){
+            inventaire.add(arme);
+        }
+    }
+    
+    public void equipementArme(String nomArme){
+        for (Arme arme : inventaire){
+            if (arme.getNom().equals(nomArme)){
+                armeEnMain = arme;
+                System.out.println(nom+ " est maintenant arme avec "+ arme.getNom());
+                return;
+            }
+        }
+        System.out.println("L'arme "+ nomArme+" n'est pas dans l'inentaire de "+ nom +". ");
+    }
+    public Arme getArmeEnMain(){
+        return armeEnMain;
        
     }
-    public int getniveauVie(){
-        return niveauVie;
+
+    public ArrayList<Arme> getInventaire() {
+        return inventaire;
     }
+    
+    
+    
+    
 
            @Override
 public String toString () {
-    String chaine;
-    chaine= nom+""+niveauVie;
+    String chaine= "Personnage : "+ nom;
+    if (armeEnMain != null){
+    chaine+= "\n Arme en main : "+ armeEnMain.toString();
+    }
     return chaine;
+}
+public static int getNombrePersonnages(){
+    return nombrePersonnages;
 }
     
 }
