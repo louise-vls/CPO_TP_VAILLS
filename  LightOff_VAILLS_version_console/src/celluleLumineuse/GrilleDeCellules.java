40df51ba882ac7cf19c421f45cb52b72fc43f8d2
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-10-25 09:29:19 +0200

    Envoie de l'exo  light off  la classe CelluleLumineuse a été ajoutée et est fonctionnelle

---
+++  LightOff_VAILLS_version_console/src/celluleLumineuse/GrilleDeCellules.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package celluleLumineuse;
+
+/**
+ *
+ * @author louis
+ */
+public class GrilleDeCellules {
+    CelluleLumineuse[][] matriceCellules;
+    
+    public GrilleDeCellules(int lignes, int colonnes){
+        matriceCellules = new CelluleLumineuse[lignes][colonnes];
+        for (int i=0; i<lignes; i++){
+            for (int j=0; j<colonnes; j++){
+                
+            }
+        }
+    }
+}
