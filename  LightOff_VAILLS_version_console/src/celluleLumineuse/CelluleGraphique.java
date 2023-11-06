620e4535eb8544895aa811c072d626633af669de
louis <louis@LAPTOP-L6AR37QI.education.lakanal.epf.fr>
2023-11-06 16:59:51 +0100

    Envoie de l'exo  light off  la classe CelluleLumineuse a été ajoutée et est fonctionnelle

---  LightOff_VAILLS_version_console/src/celluleLumineuse/CelluleGraphique.java
+++  LightOff_VAILLS_version_console/src/celluleLumineuse/CelluleGraphique.java
@@ -3,11 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package celluleLumineuse;
-
+import javax.swing.JButton;
+import java.awt.Graphics;
 /**
  *
  * @author louis
  */
 public class CelluleGraphique extends JButton {
-    
+    int largeur; // largeur en pixel de la cellule
+ int hauteur; // hauteur en pixel de la cellule
+ CelluleLumineuse celluleLumineuseAssociee; 
+ // constructeur (appelé depuis FenetrePrincipale)
+ public CelluleGraphique(CelluleLumineuse celluleLumineuseAssociee, int l,int h) {
+ this.largeur = l;
+ this.hauteur = h;
+ this.celluleLumineuseAssociee = celluleLumineuseAssociee;
+ }
+ 
+ // Methode gérant le dessin de la cellule 
+ @Override
+ protected void paintComponent(Graphics g) {
+ super.paintComponent(g); 
+ this.setText(celluleLumineuseAssociee.toString());
+ }
 }
